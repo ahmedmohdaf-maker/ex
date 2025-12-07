b4e020ab4d179e5e1eae784a955a23011caa5ee8
ahmedmohdaf-maker <ahmedmohdaf@gmail.com>
2025-12-07 13:52:44 +0200

    Fix formatting issues in usePricing.js

--- src/hooks/usePricing.js
+++ src/hooks/usePricing.js
@@ -6,7 +6,7 @@ const USDC_ADDR = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
 const fetchWithTimeout = (url, opts = {}, ms = 5000) => {
   const controller = new AbortController()
   const id = setTimeout(() => controller.abort(), ms)
-  return fetch(url, { ...opts, signal: controller.signal }). finally(() => clearTimeout(id))
+  return fetch(url, { ...opts, signal: controller. signal }).finally(() => clearTimeout(id))
 }
 
 export const usePricing = () => {
@@ -16,10 +16,10 @@ export const usePricing = () => {
   const fetch1InchQuotePrice = useCallback(async (addr, decimals = 18) => {
     try {
       const amountBN = '1' + '0'.repeat(decimals)
-      const url = `https://api.1inch.io/v5. 0/137/quote?fromTokenAddress=${addr}&toTokenAddress=${USDC_ADDR}&amount=${amountBN}`
+      const url = `https://api.1inch.io/v5.0/137/quote?fromTokenAddress=${addr}&toTokenAddress=${USDC_ADDR}&amount=${amountBN}`
       const res = await fetchWithTimeout(url, {}, 3000)
-      if (!res.ok) throw new Error('1inch non-ok')
-      const j = await res.json()
+      if (! res.ok) throw new Error('1inch non-ok')
+      const j = await res. json()
       if (! j?. toTokenAmount) throw new Error('no toTokenAmount')
       return Number(j.toTokenAmount) / 10 ** 6
     } catch (e) {
@@ -30,7 +30,7 @@ export const usePricing = () => {
 
   const fetchCoingeckoPrice = useCallback(async (addr) => {
     try {
-      const url = `https://api.coingecko.com/api/v3/simple/token_price/${COINGECKO_CHAIN}? contract_addresses=${addr}&vs_currencies=usd`
+      const url = `https://api. coingecko.com/api/v3/simple/token_price/${COINGECKO_CHAIN}?contract_addresses=${addr}&vs_currencies=usd`
       const res = await fetchWithTimeout(url, {}, 3000)
       if (!res.ok) throw new Error('cg non-ok')
       const j = await res.json()
@@ -44,7 +44,7 @@ export const usePricing = () => {
 
   const fetchDexscreenerPrice = useCallback(async (addr) => {
     try {
-      const url = `https://api.dexscreener.com/latest/dex/tokens/${addr}`
+      const url = `https://api.dexscreener. com/latest/dex/tokens/${addr}`
       const res = await fetchWithTimeout(url, {}, 3000)
       if (!res.ok) throw new Error('dexscreener non-ok')
       const j = await res.json()
@@ -73,7 +73,7 @@ export const usePricing = () => {
         (await fetchDexscreenerPrice(addr))
 
       if (price) {
-        setPriceCache(prev => new Map(prev). set(addr.toLowerCase(), { price, ts: Date.now() }))
+        setPriceCache(prev => new Map(prev).set(addr.toLowerCase(), { price, ts: Date. now() }))
       }
       setLoading(false)
       return price
